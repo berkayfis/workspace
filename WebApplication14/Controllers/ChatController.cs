8ee5be97c91724f1c374e08326311195821d45ff
Berkay Fis <fisberkay@gmail.com>
2020-04-09 22:54:52 +0300

    File paths are updated

--- WebApplication14/Controllers/ChatController.cs
+++ WebApplication14/Controllers/ChatController.cs
@@ -14,151 +14,154 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace WebApplication14.Controllers
 {
-	[Authorize]
+    [Authorize]
     public class ChatController : Controller
     {
-		private readonly AraProjeContext _context;
+        private readonly AraProjeContext _context;
 
-		public ChatController(AraProjeContext context)
-		{
-			_context = context;
-		}
-		public IActionResult Index()
+        public ChatController(AraProjeContext context)
         {
-			//if (HttpContext.Session.GetInt32("akademisyen") == null)
-			//{
-			//	return RedirectToAction("Logout", "Login");
-			//}
-			SetMessageGuid();
-			GetMessages();
-			//ViewBag.DanismaniOldugumProjeler = _context.ProjeAl.Where(x => (x.ProjeNoNavigation.DanismanId == id || x.OgrenciOneriNoNavigation.Danismanid == id) && x.KabulDurumu == "Kabul").ToList();
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            //if (HttpContext.Session.GetInt32("akademisyen") == null)
+            //{
+            //	return RedirectToAction("Logout", "Login");
+            //}
+            SetMessageGuid();
+            GetMessages();
+            //ViewBag.DanismaniOldugumProjeler = _context.ProjeAl.Where(x => (x.ProjeNoNavigation.DanismanId == id || x.OgrenciOneriNoNavigation.Danismanid == id) && x.KabulDurumu == "Kabul").ToList();
             return View();
         }
 
-		private void SetMessageGuid()
-		{
-			var userRole = User.Identity.AuthenticationType;
-			if (userRole == "Akademisyen Claims")
-			{
-				CheckAkademisyenMessageGuid();				
-			}
-			else if (userRole == "Ogrenci Claims")
-			{
-				CheckOgrenciMessageGuid();
-			}
-		}
-		private void CheckOgrenciMessageGuid()
-		{
-			string id = HttpContext.Session.GetString("id");
-			Ogrenci ogrenci = _context.Ogrenci.Find(id);
-			if (ogrenci != null && ogrenci.MessageId == Guid.Empty)
-			{
-				ogrenci.MessageId = Guid.NewGuid();
-				_context.Ogrenci.Update(ogrenci);
-				_context.SaveChanges();
-			}
-		}
-		private void CheckAkademisyenMessageGuid()
-		{
-			int id = Convert.ToInt32(HttpContext.Session.GetInt32("id"));
-			AkademikPersonel akademisyen = _context.AkademikPersonel.Find(id);
-			if (akademisyen != null && akademisyen.MessageId == Guid.Empty)
-			{
-				akademisyen.MessageId = Guid.NewGuid();
-				_context.AkademikPersonel.Update(akademisyen);
-				_context.SaveChanges();
-			}
-		}
+        private void SetMessageGuid()
+        {
+            var userRole = User.Identity.AuthenticationType;
+            if (userRole == "Akademisyen Claims")
+            {
+                CheckAkademisyenMessageGuid();
+            }
+            else if (userRole == "Ogrenci Claims")
+            {
+                CheckOgrenciMessageGuid();
+            }
+        }
+        private void CheckOgrenciMessageGuid()
+        {
+            string id = HttpContext.Session.GetString("id");
+            Ogrenci ogrenci = _context.Ogrenci.Find(id);
+            if (ogrenci != null && ogrenci.MessageId == Guid.Empty)
+            {
+                ogrenci.MessageId = Guid.NewGuid();
+                _context.Ogrenci.Update(ogrenci);
+                _context.SaveChanges();
+            }
+        }
+        private void CheckAkademisyenMessageGuid()
+        {
+            int id = Convert.ToInt32(HttpContext.Session.GetInt32("id"));
+            AkademikPersonel akademisyen = _context.AkademikPersonel.Find(id);
+            if (akademisyen != null && akademisyen.MessageId == Guid.Empty)
+            {
+                akademisyen.MessageId = Guid.NewGuid();
+                _context.AkademikPersonel.Update(akademisyen);
+                _context.SaveChanges();
+            }
+        }
 
-		[Authorize]
-		public IActionResult SendEmail() {
-			//if (HttpContext.Session.GetInt32("akademisyen") == null)
-			//{
-			//	return RedirectToAction("Logout", "Login");
-			//}
+        [Authorize]
+        public IActionResult SendEmail()
+        {
+            //if (HttpContext.Session.GetInt32("akademisyen") == null)
+            //{
+            //	return RedirectToAction("Logout", "Login");
+            //}
 
-			//int id = 1;
-			//try
-			//{
-			//	id = Convert.ToInt32(HttpContext.Request.Query["id"].ToString());
-			//}
-			//catch (NullReferenceException nre)
-			//{
-			//	return RedirectToAction("Index", "Home");
-			//}
-			//ProjeAl proje = _context.ProjeAl.FirstOrDefault(x => x.Id == id);
-			var akademisyenler = _context.AkademikPersonel.OrderBy(x => x.Ad);
-			var ogrenciler = _context.Ogrenci.OrderBy(x => x.OgrenciNo);
+            //int id = 1;
+            //try
+            //{
+            //	id = Convert.ToInt32(HttpContext.Request.Query["id"].ToString());
+            //}
+            //catch (NullReferenceException nre)
+            //{
+            //	return RedirectToAction("Index", "Home");
+            //}
+            //ProjeAl proje = _context.ProjeAl.FirstOrDefault(x => x.Id == id);
+            var akademisyenler = _context.AkademikPersonel.OrderBy(x => x.Ad);
+            var ogrenciler = _context.Ogrenci.OrderBy(x => x.OgrenciNo);
 
-			ViewBag.Akademisyenler = akademisyenler;
-			ViewBag.Ogrenciler = ogrenciler;
-			return View();
-		}
-		[HttpPost]
-		public async Task<IActionResult> SendEmailAsync(string Icerik, Guid ReceiverId, IFormFile Eklenti) {
-			if (Eklenti != null) { 
-				if (Eklenti.Length > 0)
-				{
-					// full path to file in temp location
-					string filePath = "C:\\Users\\FSA\\source\\repos\\demo\\WebApplication14\\wwwroot\\disc\\" + Eklenti.FileName;
+            ViewBag.Akademisyenler = akademisyenler;
+            ViewBag.Ogrenciler = ogrenciler;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendEmailAsync(string Icerik, Guid ReceiverId, IFormFile Eklenti)
+        {
+            if (Eklenti != null)
+            {
+                if (Eklenti.Length > 0)
+                {
+                    // full path to file in temp location
+                    string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Eklenti.FileName);
 
-					using (var stream = new FileStream(filePath, FileMode.Create))
-					{
-						await Eklenti.CopyToAsync(stream);
-					}
-				}
-			}
-			Guid senderGuid = GetCurrentUserMessageId();
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await Eklenti.CopyToAsync(stream);
+                    }
+                }
+            }
+            Guid senderGuid = GetCurrentUserMessageId();
 
-			_context.Messages.Add(new Message
-			{
-				Content = Icerik,
-				CreatedDate = DateTime.Now,
-				ReceiverId = ReceiverId,
-				SenderId =  senderGuid
-			});
-			_context.SaveChanges();			
+            _context.Messages.Add(new Message
+            {
+                Content = Icerik,
+                CreatedDate = DateTime.Now,
+                ReceiverId = ReceiverId,
+                SenderId = senderGuid
+            });
+            _context.SaveChanges();
 
-			return RedirectToAction("Index");
-		}
+            return RedirectToAction("Index");
+        }
 
-		private Guid GetCurrentUserMessageId()
-		{
-			var userRole = User.Identity.AuthenticationType;
-			if (userRole == "Akademisyen Claims")
-			{
-				int id = Convert.ToInt32(HttpContext.Session.GetInt32("id"));
-				AkademikPersonel akademisyen = _context.AkademikPersonel.Find(id);
-				return akademisyen.MessageId;
-			}
-			else if (userRole == "Ogrenci Claims")
-			{
-				string id = HttpContext.Session.GetString("id");
-				Ogrenci ogrenci = _context.Ogrenci.Find(id);
-				return ogrenci.MessageId;
-			}
-			else
-			{
-				return Guid.Empty;
-			}
-		}
+        private Guid GetCurrentUserMessageId()
+        {
+            var userRole = User.Identity.AuthenticationType;
+            if (userRole == "Akademisyen Claims")
+            {
+                int id = Convert.ToInt32(HttpContext.Session.GetInt32("id"));
+                AkademikPersonel akademisyen = _context.AkademikPersonel.Find(id);
+                return akademisyen.MessageId;
+            }
+            else if (userRole == "Ogrenci Claims")
+            {
+                string id = HttpContext.Session.GetString("id");
+                Ogrenci ogrenci = _context.Ogrenci.Find(id);
+                return ogrenci.MessageId;
+            }
+            else
+            {
+                return Guid.Empty;
+            }
+        }
 
-		private List<Guid> GetMessages()
-		{
-			try
-			{
-				Guid currentUserMessageId = GetCurrentUserMessageId();
-				var receivedMessages = _context.Messages.Where(x => x.ReceiverId == currentUserMessageId).GroupBy(x=> x.SenderId).Select(x=>x.Key).ToList();
-				var sendedMessages = _context.Messages.Where(x => x.SenderId == currentUserMessageId).GroupBy(x => x.ReceiverId).Select(x => x.Key).ToList();
+        private List<Guid> GetMessages()
+        {
+            try
+            {
+                Guid currentUserMessageId = GetCurrentUserMessageId();
+                var receivedMessages = _context.Messages.Where(x => x.ReceiverId == currentUserMessageId).GroupBy(x => x.SenderId).Select(x => x.Key).ToList();
+                var sendedMessages = _context.Messages.Where(x => x.SenderId == currentUserMessageId).GroupBy(x => x.ReceiverId).Select(x => x.Key).ToList();
 
-				return null;
-				
-			}
-			catch (Exception)
-			{
+                return null;
 
-				throw;
-			}
-		}
-	}
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
 }
--- WebApplication14/Controllers/OgrenciController.cs
+++ WebApplication14/Controllers/OgrenciController.cs
@@ -82,7 +82,7 @@ namespace WebApplication14.Controllers
             if (Form2.Length > 0)
             {
                 // full path to file in temp location
-                var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, proje.Ogrenci1No + " - " + proje.Isim + ".pdf"); //"C:\\Users\\FSA\\source\\repos\\demo\\WebApplication14\\wwwroot\\disc\\" + proje.Ogrenci1No + " - " + proje.Isim + ".pdf";
+                var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Ogrenci", proje.Ogrenci1No + " - " + proje.Isim + ".pdf"); //"C:\\Users\\FSA\\source\\repos\\demo\\WebApplication14\\wwwroot\\disc\\" + proje.Ogrenci1No + " - " + proje.Isim + ".pdf";
 
                 using (var stream = new FileStream(filePath, FileMode.Create))
                 {
@@ -243,13 +243,13 @@ namespace WebApplication14.Controllers
         {
             String ogrNo = HttpContext.Session.GetString("id");
             ProjeAl proje = ProjeAlmisMi(ogrNo);
-            String filePath = "C:\\Users\\FSA\\source\\repos\\demo\\WebApplication14\\wwwroot\\disc\\" + ogrNo + "\\";
+            string rootPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Ogrenci", ogrNo);
 
             if (AraRapor1 != null)
             {
                 if (AraRapor1.Length > 0)
                 {
-                    filePath += "AraRapor1.pdf";
+                    var filePath = Path.Combine(rootPath, "AraRapor1.pdf");
 
                     using (var stream = new FileStream(filePath, FileMode.Create))
                     {
@@ -268,7 +268,7 @@ namespace WebApplication14.Controllers
                 if (AraRapor2.Length > 0)
                 {
                     // full path to file in temp location
-                    filePath += "AraRapor2.pdf";
+                    var filePath = Path.Combine(rootPath, "AraRapor2.pdf");
 
 
                     await WriteToDiskAsync(AraRapor2, filePath);
@@ -284,7 +284,7 @@ namespace WebApplication14.Controllers
             {
                 if (FinalRaporu.Length > 0)
                 {
-                    filePath += "\\FinalRapor.pdf";
+                    var filePath = Path.Combine(rootPath, "FinalRapor.pdf");
 
                     using (var stream = new FileStream(filePath, FileMode.Create))
                     {
